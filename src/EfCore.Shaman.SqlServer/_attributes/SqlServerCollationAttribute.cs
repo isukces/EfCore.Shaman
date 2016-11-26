89767db21e4259d2adf3997e0b4af0e4810ae7c2
Piotr Stęclik <piotr@steclik.com>
2016-11-26 16:28:23 +0100

    Add nuspec file

--- src/EfCore.Shaman.SqlServer/_attributes/SqlServerCollationAttribute.cs
+++ src/EfCore.Shaman.SqlServer/_attributes/SqlServerCollationAttribute.cs
@@ -1,12 +1,33 @@
-﻿using System;
-using System.Xml.Schema;
+﻿#region using
 
-namespace EfCore.Shaman.SqlServer
+using System;
+
+#endregion
+
+// ReSharper disable once CheckNamespace
+
+namespace EfCore.Shaman
 {
-
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Property)]
     public class SqlServerCollationAttribute : Attribute
     {
+        #region Constructors
+
+        public SqlServerCollationAttribute()
+        {
+        }
+
+        public SqlServerCollationAttribute(string collation)
+        {
+            Collation = collation;
+        }
+
+        #endregion
+
+        #region Properties
+
         public string Collation { get; set; }
+
+        #endregion
     }
 }
