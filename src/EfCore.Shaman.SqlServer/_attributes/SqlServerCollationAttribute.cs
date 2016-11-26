73644beb111d4cacdfc0c4cf26c4c7974cea7abf
Piotr Stęclik <piotr@steclik.com>
2016-11-26 13:02:14 +0100

    Introducing EfCore.Shaman.SqlServer project with MsSql specific features

---
+++ src/EfCore.Shaman.SqlServer/_attributes/SqlServerCollationAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Xml.Schema;
+
+namespace EfCore.Shaman.SqlServer
+{
+
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Property)]
+    public class SqlServerCollationAttribute : Attribute
+    {
+        public string Collation { get; set; }
+    }
+}
