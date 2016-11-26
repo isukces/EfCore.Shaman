19f0dc3848775a9f181321db95650ed6e2de6dc0
Piotr Stęclik <piotr@steclik.com>
2016-11-26 17:16:41 +0100

    introduce Xunit tests

---
+++ src/EfCore.Shaman.Tests/ModelInfoTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EfCore.Shaman.ModelScanner;
+using Xunit;
+
+namespace EfCore.Shaman.Tests
+{
+    public class ModelInfoTests
+    {
+        [Fact]
+        public void ShouldRecoginzeNotNull()
+        {
+            Assert.True(ModelInfo.NotNullFromPropertyType(typeof(int)));
+            Assert.True(ModelInfo.NotNullFromPropertyType(typeof(long)));
+            Assert.True(ModelInfo.NotNullFromPropertyType(typeof(Guid)));
+
+            Assert.False(ModelInfo.NotNullFromPropertyType(typeof(int?)));
+            Assert.False(ModelInfo.NotNullFromPropertyType(typeof(long?)));
+            Assert.False(ModelInfo.NotNullFromPropertyType(typeof(Guid?)));
+
+            Assert.False(ModelInfo.NotNullFromPropertyType(typeof(string)));
+
+        }
+    }
+}
