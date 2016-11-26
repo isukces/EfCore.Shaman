78e6c9e51eb5812d39a5219643046e96e21c9190
Piotr Stęclik <piotr@steclik.com>
2016-11-26 16:00:50 +0100

    SqlServer services for reflection scanning

---
+++ src/EfCore.Shaman.SqlServer/ShamanSqlServerExtension.cs
@@ -0,0 +1,23 @@
+﻿#region using
+
+using System;
+using EfCore.Shaman.SqlServer;
+
+#endregion
+
+// ReSharper disable once CheckNamespace
+
+namespace EfCore.Shaman
+{
+    public static class ShamanSqlServerExtension
+    {
+        #region Static Methods
+
+        public static ShamanOptions WithSqlServer(this ShamanOptions options)
+        {
+            return options.With<SqlServerFixerService>();
+        }
+
+        #endregion
+    }
+}
---
+++ src/EfCore.Shaman.SqlServer/SqlServerFixerService.cs
@@ -0,0 +1,40 @@
+﻿#region using
+
+using System;
+using System.Reflection;
+using EfCore.Shaman.ModelScanner;
+
+#endregion
+
+namespace EfCore.Shaman.SqlServer
+{
+    class SqlServerFixerService : IColumnInfoUpdateService, IDbSetInfoUpdateService
+    {
+        #region Instance Methods
+
+        public void UpdateColumnInfo(ColumnInfo columnInfo, PropertyInfo propertyInfo)
+        {
+            var attribute = propertyInfo.GetCustomAttribute<SqlServerCollationAttribute>();
+            var collation = attribute?.Collation?.Trim();
+            if (string.IsNullOrEmpty(collation)) return;
+            columnInfo.Annotations[Ck] = collation;
+        }
+
+        #endregion
+
+        #region Other
+
+        public const string Prefix = "SqlServer.";
+        public const string Ck = Prefix + "Collation";
+
+        #endregion
+
+        public void UpdateDbSetInfo(DbSetInfo dbSetInfo, Type entityType)
+        {
+            var attribute = entityType.GetCustomAttribute<SqlServerCollationAttribute>();
+            var collation = attribute?.Collation?.Trim();
+            if (string.IsNullOrEmpty(collation)) return;
+            dbSetInfo.Annotations[Ck] = collation;
+        }
+    }
+}
