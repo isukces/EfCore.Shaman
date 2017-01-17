0e0240c9ff5851b6190303935c57bcaaf38c8766
Piotr Stęclik <piotr@steclik.com>
2017-01-17 10:59:03 +0100

    Hot-Fixed SimplePropertyReaderWriter (problem for creating reader or writer)

--- src/EfCore.Shaman/Reflection/SimplePropertyReaderWriter.cs
+++ src/EfCore.Shaman/Reflection/SimplePropertyReaderWriter.cs
@@ -12,38 +12,46 @@ namespace EfCore.Shaman.Reflection
     {
         #region Constructors
 
-        /// <summary>
-        /// Inspired by http://stackoverflow.com/questions/1272454/generate-dynamic-method-to-set-a-field-of-a-struct-instead-of-using-reflection
-        /// </summary>
-        /// <param name="instance"></param>
-        /// <param name="value"></param>
-        delegate void ByRefStructAction(ref object instance, object value);
         public SimplePropertyReaderWriter(Type type, PropertyInfo propertyInfo)
         {
             _propertyInfo = propertyInfo;
+            try
             {
-                var instance = Expression.Parameter(typeof(object), "instance");
-                Expression expr = Expression.Property(Expression.Convert(instance, type), propertyInfo.Name);
-                Expression convertedToObject = Expression.Convert(expr, typeof(object));
-                _reader = Expression.Lambda<Func<object, object>>(convertedToObject, instance).Compile();
+                {
+                    var instance = Expression.Parameter(typeof(object), "instance");
+                    Expression expr = Expression.Property(Expression.Convert(instance, type), propertyInfo.Name);
+                    Expression convertedToObject = Expression.Convert(expr, typeof(object));
+                    _reader = Expression.Lambda<Func<object, object>>(convertedToObject, instance).Compile();
+                }
             }
+            catch (Exception e)
             {
-                var instance = Expression.Parameter(typeof(object).MakeByRefType(), "instance");
-                var value = Expression.Parameter(typeof(object), "value");
+                // use reflection
+            }
+            try
+            {
+                {
+                    var instance = Expression.Parameter(typeof(object).MakeByRefType(), "instance");
+                    var value = Expression.Parameter(typeof(object), "value");
 
-                Expression<ByRefStructAction> expr =
-                    Expression.Lambda<ByRefStructAction>(
-                        Expression.Assign(
-                            Expression.Property(
-                                Expression.Convert(instance, type),
-                                propertyInfo),
-                            Expression.Convert(
-                                value,
-                                propertyInfo.PropertyType)),
-                        instance,
-                        value);
+                    var expr =
+                        Expression.Lambda<ByRefStructAction>(
+                            Expression.Assign(
+                                Expression.Property(
+                                    Expression.Convert(instance, type),
+                                    propertyInfo),
+                                Expression.Convert(
+                                    value,
+                                    propertyInfo.PropertyType)),
+                            instance,
+                            value);
 
-                _writer = expr.Compile();
+                    _writer = expr.Compile();
+                }
+            }
+            catch (Exception e)
+            {
+                // use reflection
             }
         }
 
@@ -53,13 +61,16 @@ namespace EfCore.Shaman.Reflection
 
         public object ReadPropertyValue(object obj)
         {
-            return _reader(obj);
+            if (_reader != null)
+                return _reader(obj);
+            return _propertyInfo.GetValue(obj);
         }
 
         public void WritePropertyValue(object obj, object value)
         {
-            _writer.Invoke(ref obj, value);
-            // _propertyInfo.SetValue(obj, value);
+            if (_writer != null)
+                _writer.Invoke(ref obj, value);
+            _propertyInfo.SetValue(obj, value);
         }
 
         #endregion
@@ -69,6 +80,18 @@ namespace EfCore.Shaman.Reflection
         private readonly Func<object, object> _reader;
         private readonly PropertyInfo _propertyInfo;
         private readonly ByRefStructAction _writer;
+
+        #endregion
+
+        #region Delegates
+
+        /// <summary>
+        ///     Inspired by
+        ///     http://stackoverflow.com/questions/1272454/generate-dynamic-method-to-set-a-field-of-a-struct-instead-of-using-reflection
+        /// </summary>
+        /// <param name="instance"></param>
+        /// <param name="value"></param>
+        private delegate void ByRefStructAction(ref object instance, object value);
 
         #endregion
     }
