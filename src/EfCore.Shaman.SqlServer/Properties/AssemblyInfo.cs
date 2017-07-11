41d0c233234c34c7d3d512f96d013f24ba11a59e
Piotr Stęclik <piotr@steclik.com>
2017-07-11 12:37:49 +0200

    nuget package release

--- src/EfCore.Shaman.SqlServer/Properties/AssemblyInfo.cs
+++ src/EfCore.Shaman.SqlServer/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("4d085deb-ec4d-4001-8d1b-fc26d2142417")]
-[assembly: AssemblyVersion("1.0.17155.56")]
-[assembly: AssemblyFileVersion("1.0.17155.56")]
+[assembly: AssemblyVersion("1.0.17192.57")]
+[assembly: AssemblyFileVersion("1.0.17192.57")]
 
     
