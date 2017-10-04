82cd9f909764561258637396bcdef04001ada4ff
Piotr Stęclik <piotr@steclik.com>
2017-10-04 08:34:28 +0200

    Fix index attribute usage

--- src/EfCore.Shaman/_attributes/IndexAttribute.cs
+++ src/EfCore.Shaman/_attributes/IndexAttribute.cs
@@ -1,5 +1,8 @@
-﻿namespace EfCore.Shaman
+﻿using System;
+
+namespace EfCore.Shaman
 {
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = true)]
     public sealed class IndexAttribute : AbstractIndexAttribute
     {
         public IndexAttribute(string name = OneFieldIndexWithAutoName, int order = 0, bool isDescending = false)
--- src/EfCore.Shaman/_attributes/UniqueIndexAttribute.cs
+++ src/EfCore.Shaman/_attributes/UniqueIndexAttribute.cs
@@ -1,5 +1,8 @@
-﻿namespace EfCore.Shaman
+﻿using System;
+
+namespace EfCore.Shaman
 {
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = true)]
     public sealed class UniqueIndexAttribute : AbstractIndexAttribute
     {
         public UniqueIndexAttribute(string name = OneFieldIndexWithAutoName, int order = 0, bool isDescending = false)
